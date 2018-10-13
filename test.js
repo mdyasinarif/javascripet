6ad646f28f447ca1bc56790d7737f57bdb4f0dae
mdyasinarif <mdyasinarifidb49.com>
2018-10-13 18:58:05 +0600

    js

---
+++ test.js
@@ -0,0 +1,13 @@
+<!-- Hello World.js-->
+document.write("Hello World"+"<br/>");
+var x=10;
+var y=15;
+var result=x+y;
+document.write("Result= "+result+"<br/>");
+var str = "Hello world"
+document.write(str);
+
+var multi = x*y;
+console.log("=========Result======="+multi);
+
+var decleration=" 10 flags"
